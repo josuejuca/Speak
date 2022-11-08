78762ee9cc93bd7b3af4714cdcfa828c3ec45fbb
Josué Juca <josuejuca19@gmail.com>
2022-11-08 04:52:13 -0300

    [ 08/11/2022 ]

---
+++ controller/server.php
@@ -0,0 +1,43 @@
+<?php
+require_once '../dao/UsuarioDAO.php';
+require_once '../dto/UsuarioDTO.php';
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Speak</title>
+</head>
+<body>
+    <?php
+        $nome = $_POST["nome"];
+        $email = $_POST["email"];
+        $login = $_POST["login"];
+        $senha = md5($_POST["senha"]);
+        $idPerfil = $_POST["idPerfil"];
+
+        if (($nome == "") or ( $email == "" ) or ($login == "") or ($senha == "") or ($idPerfil == ""))
+        {
+        echo "<script>";
+        echo "alert ('Todas as opções devem ser marcadas!');";
+        echo "window.location.href='../view/cadastro.php';";
+        echo "</script>";
+        }
+        else
+        {
+            $usarioDTO = new UsuarioDTO;
+           
+            $usarioDTO ->setEmail($email);
+            $usarioDTO ->setIdPerfil($idPerfil);
+            $usarioDTO ->setLogin($login);
+            $usarioDTO ->setNome($nome);
+            $usarioDTO ->setSenha($senha);
+           
+            $usuarioDAO = new UsuarioDAO;
+            $resutado = $usuarioDAO ->Gravar($usarioDTO);
+        }
+    ?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,21 @@
+<!-- 
+- Juca Produções
+- Criação : Juca
+- Nome do Site: Speak 
+- Descrição: A professora Sandra nos ensinou a criar um site de chat, eu diria que um app de chat, então eu peguei esse conteúdo e apliquei nesse mini projeto onde meu objetivo 
+é criar um aplicativo de chat usando tudo que ela nos passou. Em outras palavras eu diria que estou estudando, aplicando esse conteúdo em um projeto, que eu vou tentar aperfeiçoar.
+-->
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Speak</title>
+</head>
+<body>
+    <?php
+
+    ?>
+</body>
+</html>
