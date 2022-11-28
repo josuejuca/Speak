398bbb471919481574aed1417362aeddbdc77bbb
Josué Juca <josuejuca19@gmail.com>
2022-11-28 20:53:04 -0300

    [ 28/11/2022 ]

--- controller/server.php
+++ controller/server.php
@@ -9,6 +9,7 @@ require_once '../dto/UsuarioDTO.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Speak</title>
+    <meta http-equiv="refresh" content="0; url=../view/users.php">
 </head>
 <body>
     <?php
