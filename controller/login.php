21c4a0bdd15ff10b211e2ce1c6efc97db1c12d00
Josué Juca <josuejuca19@gmail.com>
2022-11-28 20:57:38 -0300

    [ 28/11/2022 ]

---
+++ controller/login.php
@@ -0,0 +1,19 @@
+<?php
+require_once '../dao/UsuarioDAO.php';
+require_once '../dto/UsuarioDTO.php';
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Speak - Login</title>
+    <!-- <meta http-equiv="refresh" content="0; url=../view/users.php"> -->
+</head>
+<body>
+    <?php
+        
+    ?>
+</body>
+</html>
